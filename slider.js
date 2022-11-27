11bc93780ac70d76d229faa0e522976a8f2806c1
aayat <aayatbilal08@gmail.com>
2022-11-27 16:56:13 +0530

    new rapo

---
+++ slider.js
@@ -0,0 +1,9 @@
+function one(){
+    document.getElementById("slideimage").src="img1.jpeg";
+}
+function two(){
+    document.getElementById("slideimage").src="img2.jpeg";
+}
+function three(){
+    document.getElementById("slideimage").src="img3.jpeg";
+}
